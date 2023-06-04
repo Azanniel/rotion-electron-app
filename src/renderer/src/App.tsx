f687fdbba96b18978f983fe19cb9ad788a3d33f4
azanniel <leo.azannielttt@gmail.com>
2023-06-04 17:13:06 -0400

    💄 add tailwindcss in renderer layer

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,3 @@
 export function App() {
-  return <h1 style={{ fontFamily: 'Inter' }}>Hello world, Rotion</h1>
+  return <h1 className="text-violet-400 font-bold text-4xl">Hello world, Rotion</h1>
 }
---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
