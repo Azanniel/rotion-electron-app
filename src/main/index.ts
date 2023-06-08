5088a7f68357d7b6f8d0e3ba681357290793b55f
azanniel <leo.azannielttt@gmail.com>
2023-06-08 16:18:41 -0400

    🧱 add structure of components in app

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,6 +11,11 @@ function createWindow() {
     show: false,
     autoHideMenuBar: true,
     backgroundColor: '#17141f',
+    titleBarStyle: 'hiddenInset',
+    trafficLightPosition: {
+      x: 20,
+      y: 20,
+    },
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: path.join(__dirname, '../preload/index.js'),
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,18 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
 export function App() {
-  return <div className="bg-rotion-900 text-rotion-100 h-screen w-screen"></div>
+  return (
+    <div className="text-rotion-100 flex h-screen w-screen">
+      <Sidebar />
+
+      <div className="flex max-h-screen flex-1 flex-col">
+        <Header />
+
+        <main className="text-rotion-400 flex flex-1 items-center justify-center">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
+  )
 }
