e798da53db595c3b7c98e1708b15be4ae39ea7a9
azanniel <leo.azannielttt@gmail.com>
2023-06-17 15:29:06 -0400

    ✨ create new document with react query

--- src/renderer/src/components/Sidebar/CreatePage.tsx
+++ src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -1,8 +1,38 @@
 import { Plus } from 'phosphor-react'
+import { useMutation, useQueryClient } from 'react-query'
+import { Document } from '@shared/types/ipc'
 
 export function CreatePage() {
+  const queryClient = useQueryClient()
+
+  const {
+    isLoading: isCreatingNewDocument,
+    mutateAsync: handleCreateDocument,
+  } = useMutation(
+    async () => {
+      const response = await window.api.createDocument()
+
+      return response.data
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.setQueryData<Document[]>(['documents'], (documents) => {
+          if (!documents) {
+            return [data]
+          }
+
+          return [...documents, data]
+        })
+      },
+    },
+  )
+
   return (
-    <button className="border-rotion-600 hover:bg-rotion-700 absolute bottom-0 left-0 right-0 flex w-[240px] items-center gap-2 border-t px-5 py-4 text-sm disabled:opacity-60">
+    <button
+      onClick={() => handleCreateDocument()}
+      disabled={isCreatingNewDocument}
+      className="border-rotion-600 hover:bg-rotion-700 absolute bottom-0 left-0 right-0 flex w-[240px] items-center gap-2 border-t px-5 py-4 text-sm disabled:opacity-60"
+    >
       <Plus className="h-4 w-4" />
       Novo documento
     </button>
