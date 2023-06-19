7aa7ba93ce173daf74128bc27379a6bf81f408ce
azanniel <leo.azannielttt@gmail.com>
2023-06-19 12:32:00 -0400

    ✨ add cmk search

--- src/renderer/src/components/SearchBar/index.tsx
+++ src/renderer/src/components/SearchBar/index.tsx
@@ -1,26 +1,43 @@
+import { useEffect } from 'react'
+import { useQuery } from 'react-query'
 import { Command } from 'cmdk'
+import { useNavigate } from 'react-router-dom'
 import { File, MagnifyingGlass } from 'phosphor-react'
-import { useEffect, useState } from 'react'
 
-export function SearchBar() {
-  const [open, setOpen] = useState(false)
+interface SearchBarProps {
+  open: boolean
+  onOpenChange: (isOpen: boolean) => void
+}
+
+export function SearchBar({ open, onOpenChange }: SearchBarProps) {
+  const navigate = useNavigate()
+
+  const { data } = useQuery(['documents'], async () => {
+    const response = await window.api.fetchDocuments()
+    return response.data
+  })
+
+  function handleOpenDocument(id: string) {
+    navigate(`/documents/${id}`)
+    onOpenChange(false)
+  }
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === 'k' && e.metaKey) {
-        setOpen((state) => !state)
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        onOpenChange(!open)
       }
     }
 
     document.addEventListener('keydown', down)
     return () => document.removeEventListener('keydown', down)
-  }, [setOpen])
+  }, [onOpenChange, open])
 
   return (
     <Command.Dialog
       className="bg-rotion-800 text-rotion-100 border-rotion-600 fixed left-1/2 top-24 w-[480px] max-w-full -translate-x-1/2 rounded-md border shadow-2xl"
       open={open}
-      onOpenChange={setOpen}
+      onOpenChange={onOpenChange}
       label="Search"
     >
       <div className="border-rotion-700 flex items-center gap-2 border-b p-4">
@@ -31,30 +48,24 @@ export function SearchBar() {
           className="text-rotion-50 placeholder:text-rotion-200 w-full bg-transparent text-sm focus:outline-none"
         />
       </div>
-      <Command.List className="scrollbar-thin scrollbar-thumb-rotion-600 scrollbar-track-rotion-800 max-h-48 py-2">
+
+      <Command.List className="scrollbar-thin scrollbar-thumb-rotion-600 scrollbar-track-rotion-800 max-h-48 overflow-y-auto">
         <Command.Empty className="text-rotion-200 px-4 py-3 text-sm">
           Nenhum documento encontrado.
         </Command.Empty>
 
-        <Command.Item className="text-rotion-50 hover:bg-rotion-700 aria-selected:!bg-rotion-600 flex items-center gap-2 px-4 py-3 text-sm">
-          <File className="h-4 w-4" />
-          Untitled
-        </Command.Item>
-
-        <Command.Item className="text-rotion-50 hover:bg-rotion-700 aria-selected:!bg-rotion-600 flex items-center gap-2 px-4 py-3 text-sm">
-          <File className="h-4 w-4" />
-          Ignite
-        </Command.Item>
-
-        <Command.Item className="text-rotion-50 hover:bg-rotion-700 aria-selected:!bg-rotion-600 flex items-center gap-2 px-4 py-3 text-sm">
-          <File className="h-4 w-4" />
-          Discover
-        </Command.Item>
-
-        <Command.Item className="text-rotion-50 hover:bg-rotion-700 aria-selected:!bg-rotion-600 flex items-center gap-2 px-4 py-3 text-sm">
-          <File className="h-4 w-4" />
-          Rocketseat
-        </Command.Item>
+        {data?.map((document) => {
+          return (
+            <Command.Item
+              key={document.id}
+              onSelect={() => handleOpenDocument(document.id)}
+              className="text-rotion-50 hover:bg-rotion-700 aria-selected:!bg-rotion-600 flex items-center gap-2 px-4 py-3 text-sm"
+            >
+              <File className="h-4 w-4" />
+              {document.title}
+            </Command.Item>
+          )
+        })}
       </Command.List>
     </Command.Dialog>
   )
--- src/renderer/src/components/Sidebar/Search.tsx
+++ src/renderer/src/components/Sidebar/Search.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import { MagnifyingGlass } from 'phosphor-react'
+import { SearchBar } from '../SearchBar'
 
 export function Search() {
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState(false)
+
+  function handleOpenChange(isOpen: boolean) {
+    setIsSearchBarOpen(isOpen)
+  }
+
   return (
-    <button className="text-rotion-100 hover:text-rotion-50 mx-5 flex items-center gap-2 text-sm">
-      <MagnifyingGlass className="h-5 w-5" />
-      Busca rápida
-    </button>
+    <>
+      <button
+        onClick={() => handleOpenChange(true)}
+        className="text-rotion-100 hover:text-rotion-50 mx-5 flex items-center gap-2 text-sm"
+      >
+        <MagnifyingGlass className="h-5 w-5" />
+        Busca rápida
+      </button>
+
+      <SearchBar open={isSearchBarOpen} onOpenChange={handleOpenChange} />
+    </>
   )
 }
